149443fa67a968f77bb6aa367d2a1c6b4d2f22be
Joselin Maria J <jjoselinmaria@gmail.com>
2024-06-11 19:29:34 +0530

    Update insert operation.py

--- ARRAYS/insert operation.py
+++ ARRAYS/insert operation.py
@@ -9,3 +9,15 @@ print(arr)
 a=insert(arr,ele)
 print("after insert")
 print(a)
+
+#insert at a given position
+def insert2(original_list, position, element):
+    if position<0 or position> len(original_list):
+        raise IndexError("invalid position");
+    new_list = original_list[:position] + [element] + original_list[position:]
+    return new_list;
+
+original_list = [1,2,3,4,5,6]
+position = 2
+element = 13
+print(insert2(original_list,position,element))
